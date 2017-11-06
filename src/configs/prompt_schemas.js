c48962bca1067015315aae11ea41dfdd3b896270
Marco <macrozone@gmail.com>
2017-11-06 12:05:10 +0100

    bump

--- src/configs/prompt_schemas.js
+++ src/configs/prompt_schemas.js
@@ -1,6 +1,5 @@
-environmentSchemaimport _ from 'lodash';
+import _ from 'lodash';
 import prompt from 'prompt';
-
 
 const withDefaults = (schema, defaults = {}) => ({
   ...schema,
@@ -10,76 +9,84 @@ const withDefaults = (schema, defaults = {}) => ({
   })),
 });
 
-export const initSchema = config => withDefaults({
-  properties: {
-    customer: {
-      description: 'Customer kürzel',
-      required: true,
-      default: 'pan',
+export const initSchema = config =>
+  withDefaults(
+    {
+      properties: {
+        customer: {
+          description: 'Customer kürzel',
+          required: true,
+          default: 'pan',
+        },
+        appname: {
+          description: 'App name (for dbs, filenames, etc.)',
+          type: 'string',
+          required: true,
+          pattern: /^[a-zA-Z]+$/,
+        },
+        passPath: {
+          description: 'Path in pass',
+          required: true,
+          default: () => `${prompt.history('customer').value}/${prompt.history('appname').value}`,
+        },
+        appDir: {
+          description: 'app directory',
+          type: 'string',
+          default: './app',
+        },
+        buildDir: {
+          description: 'build directory',
+          type: 'string',
+          default: './build',
+        },
+        androidBuildToolVersion: {
+          description: 'android build tool version',
+          type: 'string',
+          default: '25.0.2',
+        },
+        useYarn: {
+          description: 'use yarn to build (false: use npm)',
+          type: 'boolean',
+          default: true,
+        },
+      },
     },
-    appname: {
-      description: 'App name (for dbs, filenames, etc.)',
-      type: 'string',
-      required: true,
-      pattern: /^[a-zA-Z]+$/,
-    },
-    passPath: {
-      description: 'Path in pass',
-      required: true,
-      default: () => `${prompt.history('customer').value}/${prompt.history('appname').value}`,
-    },
-    appDir: {
-      description: 'app directory',
-      type: 'string',
-      default: './app',
-    },
-    buildDir: {
-      description: 'build directory',
-      type: 'string',
-      default: './build',
-    },
-    androidBuildToolVersion: {
-      description: 'android build tool version',
-      type: 'string',
-      default: '25.0.2',
-    },
-    useYarn: {
-      description: 'use yarn to build (false: use npm)',
-      type: 'boolean',
-      default: true,
-    },
-  },
-}, config);
+    config,
+  );
 
-export const environmentSchema = ({ environment, appname, ...config }) => withDefaults({
-  properties: {
-    host: {
-      description: 'ssh host (deprecated, only for classic hosting)',
-      type: 'string',
-      required: true,
-      default: `${appname}-${environment}.panter.biz`,
+export const environmentSchema = ({ environment, appname, ...config }) =>
+  withDefaults(
+    {
+      properties: {
+        host: {
+          description: 'ssh host (deprecated, only for classic hosting)',
+          type: 'string',
+          required: true,
+          default: `${appname}-${environment}.panter.biz`,
+        },
+        user: {
+          description: 'ssh user (deprecated, only for classic hosting)',
+          default: 'app',
+        },
+        url: {
+          description: 'full url',
+          default: () => `https://${prompt.history('host').value}`,
+        },
+        androidKeystore: {
+          description: 'android keystore file',
+          type: 'string',
+          default: './android.keystore',
+        },
+        androidKeyname: {
+          description: 'Android keystore name / alias',
+          default: `${appname}-${environment}`,
+        },
+        androidDName: {
+          description: 'android dname for key',
+          type: 'string',
+          default: () => 'cn=Panter, ou=Panter, o=Panter, c=CH',
+        },
+      },
     },
-    user: {
-      description: 'ssh user (deprecated, only for classic hosting)',
-      default: 'app',
-    },
-    url: {
-      description: 'full url',
-      default: () => `https://${prompt.history('host').value}`,
-    },
-    androidKeystore: {
-      description: 'android keystore file',
-      type: 'string',
-      default: './android.keystore',
-    },
-    androidKeyname: {
-      description: 'Android keystore name / alias',
-      default: `${appname}-${environment}`,
-    },
-    androidDName: {
-      description: 'android dname for key',
-      type: 'string',
-      default: () => 'cn=Panter, ou=Panter, o=Panter, c=CH',
-    },
-  },
-}, _.get(config, ['environments', environment]));
+    _.get(config, ['environments', environment]),
+  );
